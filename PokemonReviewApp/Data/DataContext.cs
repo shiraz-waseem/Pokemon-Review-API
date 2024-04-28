4905ec684f5193c1a2e546bb848c47a432a9c1ec
shirazwaseem <shiraz.waseem@octdaily.com>
2024-04-28 13:20:32 -0700

    Installing Entity FrameWork

---
+++ PokemonReviewApp/Data/DataContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Data
+{
+    public class DataContext : DbContext
+    {
+        // base is like pushing data from DbContext to where you are inheriting
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        // Our tables
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Pokemon> Pokemon { get; set; }
+        public DbSet<PokemonOwner> PokemonOwners { get; set; }
+        public DbSet<PokemonCategory> PokemonCategories { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Reviewer> Reviewers { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PokemonCategory>()
+                .HasKey(pc => new { pc.PokemonId, pc.CategoryId });       // EF we need to link these two ids
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(pc => pc.PokemonCategories)
+                .HasForeignKey(p => p.PokemonId);
+            modelBuilder.Entity<PokemonCategory>()
+               .HasOne(c => c.Category)
+               .WithMany(pc => pc.PokemonCategories)
+               .HasForeignKey(c => c.CategoryId);
+
+
+            modelBuilder.Entity<PokemonOwner>()
+               .HasKey(po => new { po.PokemonId, po.OwnerId });       // EF we need to link these two ids
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(pc => pc.PokemonOwners)
+                .HasForeignKey(p => p.PokemonId);
+            modelBuilder.Entity<PokemonOwner>()
+               .HasOne(p => p.Owner)
+               .WithMany(pc => pc.PokemonOwners)
+               .HasForeignKey(c => c.OwnerId);
+        }
+    }
+}
--- PokemonReviewApp/obj/Debug/net6.0/PokemonReviewApp.AssemblyInfo.cs
+++ PokemonReviewApp/obj/Debug/net6.0/PokemonReviewApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3816d11eb2068bfb23706b3e87fb7fe9f5b137d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72536e5b3f4bbb51cef59f8942836c684376a98b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
