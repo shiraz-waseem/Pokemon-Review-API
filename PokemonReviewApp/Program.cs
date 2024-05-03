fa9d206dbb283b810b2607ad5ae95d9ebc76418b
shirazwaseem <shiraz.waseem@octdaily.com>
2024-05-03 09:00:35 -0700

    Repository Pattern & Dependency Injection Explained

--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using PokemonReviewApp;
 using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Repository;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddTransient<Seed>();
+
+// Dependency Injection
+builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
